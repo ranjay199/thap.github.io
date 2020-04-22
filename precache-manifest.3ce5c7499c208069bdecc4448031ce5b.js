e2f9e117b0d9a710582b82e17b34b0468ba515b7
ranjay199 <44604453+ranjay199@users.noreply.github.com>
2020-04-22 14:23:36 +0530

    new web

---
+++ precache-manifest.3ce5c7499c208069bdecc4448031ce5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c301a79b6f79100d307c7061c3af246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a921958a2220cc46b86",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "ba5092fbf22b99a08435",
+    "url": "/static/css/main.915dc28e.chunk.css"
+  },
+  {
+    "revision": "0a921958a2220cc46b86",
+    "url": "/static/js/2.f5befa13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f5befa13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5092fbf22b99a08435",
+    "url": "/static/js/main.7807611b.chunk.js"
+  },
+  {
+    "revision": "c3dd3b26c06f4787e38e",
+    "url": "/static/js/runtime-main.1d6ac0dc.js"
+  }
+]);
